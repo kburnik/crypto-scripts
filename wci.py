77d4bd59e693fa9c41b0531eb10d14ec7d0b47d8
Kristijan Burnik <kristijanburnik@gmail.com>
2018-04-24 20:32:24 +0200

    Add the btc script for WorldCoinIndex.

---
+++ wci.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+# Displays the crypto ticker from worldcoinindex.com in your terminal.
+#
+# Install requirements.
+#
+# $ pip install requests si-prefix
+#
+# Grab your key at:
+#   https://www.worldcoinindex.com/apiservice
+#
+# Add to .bashrc:
+#   export WORLD_COIN_INDEX_API_KEY="yourkey"
+#   export WORLD_COIN_INDEX_MAX_ITEMS=40
+#   alias btc="python $HOME/crypto-scripts/wci.py"
+#
+# Start using:
+# $  source ~/.bashrc
+# $  btc
+
+import json
+import requests
+import os
+import sys
+from si_prefix import si_format
+
+url = ('https://www.worldcoinindex.com/apiservice/getmarkets' +
+       '?key=%(WORLD_COIN_INDEX_API_KEY)s&fiat=USD') % os.environ
+
+max_items = os.environ.get('WORLD_COIN_INDEX_MAX_ITEMS', 40)
+
+ticker = requests.get(url).json()
+
+if not 'Markets' in ticker:
+  print ticker
+  sys.exit(1)
+
+data = ticker['Markets'][0]
+data = sorted(data, key=lambda item: -item['Volume_24h'])
+
+print "%20s %9s    %12s   %10s" % ('Name', 'Market', 'Price', 'Volume 24h')
+
+for item in data[0:max_items]:
+ print "%20s %9s    $ %10.4f   %10s" % (item['Name'], item['Label'], item['Price'], si_format(item['Volume_24h']))
